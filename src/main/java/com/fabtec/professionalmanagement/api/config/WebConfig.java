56746537ce5d6658a7cee478024a6c1744f03b70
Fabrício Ricarte Magalhães <fabricio_ricarte@hotmail.com>
2023-03-10 21:00:15 -0300

    commit correções

--- src/main/java/com/fabtec/professionalmanagement/api/config/WebConfig.java
+++ src/main/java/com/fabtec/professionalmanagement/api/config/WebConfig.java
@@ -1,9 +1,11 @@
 package com.fabtec.professionalmanagement.api.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer{
 
 	@Value("${cors.originPattenrs:default}")
